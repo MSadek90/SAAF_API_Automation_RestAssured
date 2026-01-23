373ab9440d27b288cd6b1dc09d485b6bc16c39c3
MSadek90 <mhmod.sadek2017@gmail.com>
2026-01-23 09:49:22 +0200

    "update endpoints variable in service_file"

--- saaf/src/main/java/com/api/Service/ProjectsService.java
+++ saaf/src/main/java/com/api/Service/ProjectsService.java
@@ -13,29 +13,29 @@ public class ProjectsService {
 
     // Create a new project
     public Response createProject(RealEstateProjectPostRequest request){
-        return RestClient.post(AllEndPoints.Create_New_Project, request);
+        return RestClient.post(AllEndPoints.PROJECTS_STORE, request);
     }
 
     // Get project details by ID
     public Response getProjectDetails(int projectId) {
-        return RestClient.get(AllEndPoints.Get_Project, "project_id", projectId);
+        return RestClient.get(AllEndPoints.PROJECTS_SHOW, "project_id", projectId);
     }
 
 
     //link project to fund
     public Response linkProjectToFund(LinkedProjectToFundPostRequest request) {
-        return RestClient.post(AllEndPoints.Linked_Project_ToFund, request);
+        return RestClient.post(AllEndPoints.PROJECTS_LINK_FUND, request);
     }
 
 
     //Ready for use action
     public Response readyForUseAction(int value) {
-        return RestClient.put(AllEndPoints.ReadyForUse_Action, "project_id", value);
+        return RestClient.put(AllEndPoints.PROJECTS_PHASE_UPDATE, "project_id", value);
     }
 
     //Sell project action
     public Response sellProjectAction(SellprojectPostRequest request, int value) {
-        return RestClient.postWithId(AllEndPoints.Sell_Project, request, "project_id", value);
+        return RestClient.postWithId(AllEndPoints.PROJECTS_SELL, request, "project_id", value);
     }
 
 }
